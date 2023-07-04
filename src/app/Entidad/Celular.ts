1b6d60c1339f27c55f88a555d5a4c2ca4a97331c
RICARDO <ricardoseveriano14@gmail.com>
2023-07-04 13:04:04 -0600

    Mi primer Comit desde Angular

---
+++ src/app/Entidad/Celular.ts
@@ -0,0 +1,8 @@
+export class Celular{
+    idCelular !: number;
+    marca !:string;
+    modelo !:string;
+    color !:string;
+    precio !:number;
+    capacidad !:number;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditarComponent } from './Componentes/editar/editar.component';
+import { GuardarComponent } from './Componentes/guardar/guardar.component';
+import { EliminarComponent } from './Componentes/eliminar/eliminar.component';
+import { ListarComponent } from './Componentes/listar/listar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceService } from './Servicios/service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditarComponent,
+    GuardarComponent,
+    EliminarComponent,
+    ListarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
